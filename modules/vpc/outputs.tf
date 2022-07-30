39d559753dcfeb1104bb060b2f5fec79adf2c292
bhouck-aws <brooks.houck@gmail.com>
2022-07-30 14:26:29 +0000

    removing bastion asg

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,14 +2,16 @@ output "vpc" {
   value = module.vpc
 }
 
-output "webserver-sg" {
-  value = aws_security_group.webserver-sg
+output "jenkins-sg" {
+  value = aws_security_group.jenkins-sg
 }
 
 output "vpc_zone_identifier" {
   value = module.vpc.vpc_id
 }
 
+/*
 output "bastion-host-sg" {
   value = aws_security_group.bastion-host-sg
 }
+*/
