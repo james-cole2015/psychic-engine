6cd883565eb48e1e17f7c04d3c09063e36bfb551
Ubuntu <ubuntu@ip-172-31-1-64.ec2.internal>
2022-08-02 12:49:54 +0000

    adding outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "random_id_num" {
+  value = module.aws_data.random_number.result
+}
+
+output "iam-user-name" {
+  value = module.aws_data.iam-user.user_name
+}
+
+output "iam-user-arn" {
+  value = module.aws_data.iam-user.arn
+}
+
+output "random_pet" {
+  value = module.aws_data.petname.id
+}
