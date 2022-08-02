36883ed3885c119a07f2ff1594664b3b36a5233e
Ubuntu <ubuntu@ip-172-31-1-64.ec2.internal>
2022-08-02 12:50:15 +0000

    adding user-name variable

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "repo-name" {
-    type = string
-    default = "psychic-engine"
+  type    = string
+  default = "psychic-engine"
 }
 
 variable "region" {
-    type = string
-    default = "us-east-1"
+  type    = string
+  default = "us-east-1"
+}
+
+variable "user-name" {
+  type    = string
+  default = "AveryClark"
 }
