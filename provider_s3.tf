ce55f42f10c0afabdd846f0e71a6c18d214c7da1
Brooks <ubuntu@ip-172-31-1-64.ec2.internal>
2022-08-02 14:42:50 +0000

    adding avery profile

--- provider_s3.tf
+++ provider_s3.tf
@@ -19,4 +19,5 @@ provider "aws" {
   region                   = var.region
   shared_credentials_files = ["~/.aws/credentials"]
   shared_config_files      = ["~/.aws/config"]
+  profile                  = "avery"
 }
